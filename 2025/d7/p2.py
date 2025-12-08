11bbd6248b9317ce95f95006a69b1f8ec3373627
Felipe Jara <fjararibet@gmail.com>
2025-12-07 22:18:44 -0300

    2025 day 7

--- 2025/d7/p2.py
+++ 2025/d7/p2.py
@@ -1,4 +1,5 @@
 import sys
+from functools import cache
 
 
 def myfind(arr, pos):
@@ -6,6 +7,12 @@ def myfind(arr, pos):
         if p == pos:
             return i
     return None
+
+
+def pp_grid(grid, n, m):
+    for i in range(n):
+        print(''.join(grid[i, j] for j in range(m)))
+    print()
 
 
 input = open(sys.argv[1]).read()
@@ -19,7 +26,7 @@ for i, line in enumerate(input.splitlines()):
 visited = set()
 q = [(start, 1)]
 timeline_count = 1
-
+d_grid = grid.copy()
 while q:
     curr, weight = q.pop()
     if curr in visited:
@@ -36,8 +43,21 @@ while q:
                 q[n] = (p, w + weight)
             else:
                 q.insert(0, ((i, y), weight))
-
         timeline_count += weight
     else:
+        d_grid[curr] = str(weight)
         q.insert(0, ((i+1, j), weight))
+
+
+@cache
+def solve(i, j):
+    if (i, j) not in grid:
+        return 1
+    if grid[i, j] == '^':
+        return solve(i, j-1) + solve(i, j+1)
+    else:
+        return solve(i+1, j)
+
+
 print(timeline_count)
+print(solve(start[0], start[1]))
